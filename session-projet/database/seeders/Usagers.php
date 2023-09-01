82e850c4d333820cd2cc55bb42600f22d81c9a89
Antoine-Beaudry0 <antoine.beaudry.01@edu.cegeptr.qc.ca>
2023-09-01 13:14:54 -0400

    [main] Ajout des Seeders

---
+++ session-projet/database/seeders/Usagers.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Usagers extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('usagers')->insert([
+
+            [               
+                'nom' => 'N/A',
+                'num_superieur' => '',
+                'position' => '',
+                'droit_employe' => 'n',
+                'droit_superieur' => 'n'
+                'droit_admin' => 'n'
+            ],
+            [               
+                'nom' => 'Marc Pépin',
+                'num_superieur' => '2',
+                'position' => 'Journalier TP',
+                'droit_employe' => 'o',
+                'droit_superieur' => 'n'
+                'droit_admin' => 'n'
+            ],
+            [               
+                'nom' => 'Jane Dow',
+                'num_superieur' => '3',
+                'position' => 'Cheffe d\'équipe TP',
+                'droit_employe' => 'o',
+                'droit_superieur' => 'o'
+                'droit_admin' => 'n'
+            ],
+            [               
+                'nom' => 'Jonathan Morinville',
+                'num_superieur' => 'Alain Lizotte',
+                'position' => 'Chef de service TP',
+                'droit_employe' => 'o',
+                'droit_superieur' => 'o'
+                'droit_admin' => 'o'
+            ],
+            [               
+                'nom' => 'Alain Lizotte',
+                'num_superieur' => '0',
+                'position' => 'Chef de service TP',
+                'droit_employe' => 'o',
+                'droit_superieur' => 'o'
+                'droit_admin' => 'n'
+            ],
+            ],
+        ]); 
+    }
+}
