66932d1ed67c9f4b34dd1502812482aba8d7fd86
Antoine-Beaudry0 <antoine.beaudry.01@edu.cegeptr.qc.ca>
2023-09-01 13:25:13 -0400

    [Antoine] retrait vendor et .env dans gitignore

--- session-projet/database/seeders/Usagers.php
+++ session-projet/database/seeders/Usagers.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class Usagers extends Seeder
 {
@@ -19,7 +20,7 @@ class Usagers extends Seeder
                 'num_superieur' => '',
                 'position' => '',
                 'droit_employe' => 'n',
-                'droit_superieur' => 'n'
+                'droit_superieur' => 'n',
                 'droit_admin' => 'n'
             ],
             [               
@@ -27,7 +28,7 @@ class Usagers extends Seeder
                 'num_superieur' => '2',
                 'position' => 'Journalier TP',
                 'droit_employe' => 'o',
-                'droit_superieur' => 'n'
+                'droit_superieur' => 'n',
                 'droit_admin' => 'n'
             ],
             [               
@@ -35,7 +36,7 @@ class Usagers extends Seeder
                 'num_superieur' => '3',
                 'position' => 'Cheffe d\'équipe TP',
                 'droit_employe' => 'o',
-                'droit_superieur' => 'o'
+                'droit_superieur' => 'o',
                 'droit_admin' => 'n'
             ],
             [               
@@ -43,7 +44,7 @@ class Usagers extends Seeder
                 'num_superieur' => 'Alain Lizotte',
                 'position' => 'Chef de service TP',
                 'droit_employe' => 'o',
-                'droit_superieur' => 'o'
+                'droit_superieur' => 'o',
                 'droit_admin' => 'o'
             ],
             [               
@@ -51,9 +52,8 @@ class Usagers extends Seeder
                 'num_superieur' => '0',
                 'position' => 'Chef de service TP',
                 'droit_employe' => 'o',
-                'droit_superieur' => 'o'
+                'droit_superieur' => 'o',
                 'droit_admin' => 'n'
-            ],
             ],
         ]); 
     }
