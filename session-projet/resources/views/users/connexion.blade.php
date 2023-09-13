12f354447698e2a2ce5047c2dd691da9f2047427
Brice Clain <br.clain@gmail.com>
2023-09-13 10:00:28 -0400

    form pas fini

--- session-projet/resources/views/users/connexion.blade.php
+++ session-projet/resources/views/users/connexion.blade.php
@@ -4,33 +4,36 @@
  
 @section('Mid')
     <!-- MAIN CONTAINER -->
-    <section class="main-container" >
-      <div class="location info" id="home">
-            <br><br><br>
-       <h2 >Ajouter un film </h2>
+<section class="fullscreen connexion" >
+
+  <div class="contain">
+    <img src="{{ asset('img/logoVille.png') }}" alt="Logo image">
+
+
+      <form action="/login" method="post" >
+      @csrf
+      <h2 >Connexion</h2>
+        <label for="user">nom d'utilisateur</label>
+        <input type="text" id="nom" name="nom">
+        <label for="password">password</label>
+        <input type="password" id="password" name="password">
       
-<form action="/login" method="post" >
-@csrf
-  <label for="user">nom d'utilisateur</label>
-  <input type="text" id="nom" name="nom">
-  <label for="password">password</label>
-  <input type="password" id="password" name="password">
-
-  @if ($errors->any())
-                    <div>
-                        @foreach ($errors->all() as $error)
-                        <li class="text-red-500 list-none">
-                            {{$error}}
-
-                        </li>
-
-                        @endforeach
-                    </div>
-                @endif
-
-                
-  <button class="button">Se connecter</button>
-</form>
+        @if ($errors->any())
+                          <div>
+                              @foreach ($errors->all() as $error)
+                              <li class="text-red-500 list-none">
+                                  {{$error}}
+      
+                              </li>
+      
+                              @endforeach
+                          </div>
+                      @endif
+      
+                      
+        <button class="button">Se connecter</button>
+      </form>
+  </div>
                               
       </div>
 @endsection
