620eae1d1cb7b36979ff47e5595fffaa94d279b6
Brice Clain <br.clain@gmail.com>
2023-09-15 13:18:26 -0400

    page home

--- session-projet/resources/views/users/index.blade.php
+++ session-projet/resources/views/users/index.blade.php
@@ -1,21 +1,87 @@
-
 @extends('layouts.app')
- 
+
 @section('Mid')
+    <section style="">
+        <div class="contain">
+            <h2>Accueil</h2>
+            <h4 style="">Remplir un formulaire</h4>
+            <div class="forms-home">
+                <a href="" class="form-it">
+                    <svg width="22" height="27" viewBox="0 0 22 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M9.625 21.6H12.375V17.55H16.5V14.85H12.375V10.8H9.625V14.85H5.5V17.55H9.625V21.6ZM2.75 27C1.99375 27 1.34613 26.7354 0.807127 26.2062C0.268127 25.677 -0.000914332 25.0416 2.33447e-06 24.3V2.7C2.33447e-06 1.9575 0.269502 1.32165 0.808502 0.792452C1.3475 0.263252 1.99467 -0.000897708 2.75 2.29202e-06H13.75L22 8.1V24.3C22 25.0425 21.7305 25.6783 21.1915 26.2075C20.6525 26.7367 20.0053 27.0009 19.25 27H2.75ZM12.375 9.45H19.25L12.375 2.7V9.45Z"
+                            fill="#0076D5" />
+                    </svg>
+                    <h5>Formulaire de déclaration d'accident</h5>
+                </a>
+                <a href="" class="form-it">
+                    <svg width="22" height="27" viewBox="0 0 22 27" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M9.625 21.6H12.375V17.55H16.5V14.85H12.375V10.8H9.625V14.85H5.5V17.55H9.625V21.6ZM2.75 27C1.99375 27 1.34613 26.7354 0.807127 26.2062C0.268127 25.677 -0.000914332 25.0416 2.33447e-06 24.3V2.7C2.33447e-06 1.9575 0.269502 1.32165 0.808502 0.792452C1.3475 0.263252 1.99467 -0.000897708 2.75 2.29202e-06H13.75L22 8.1V24.3C22 25.0425 21.7305 25.6783 21.1915 26.2075C20.6525 26.7367 20.0053 27.0009 19.25 27H2.75ZM12.375 9.45H19.25L12.375 2.7V9.45Z"
+                            fill="#0076D5" />
+                    </svg>
+                    <h5> Audit SST</h5>
+                </a>
+                <a href="" class="form-it">
+                    <svg width="22" height="27" viewBox="0 0 22 27" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M9.625 21.6H12.375V17.55H16.5V14.85H12.375V10.8H9.625V14.85H5.5V17.55H9.625V21.6ZM2.75 27C1.99375 27 1.34613 26.7354 0.807127 26.2062C0.268127 25.677 -0.000914332 25.0416 2.33447e-06 24.3V2.7C2.33447e-06 1.9575 0.269502 1.32165 0.808502 0.792452C1.3475 0.263252 1.99467 -0.000897708 2.75 2.29202e-06H13.75L22 8.1V24.3C22 25.0425 21.7305 25.6783 21.1915 26.2075C20.6525 26.7367 20.0053 27.0009 19.25 27H2.75ZM12.375 9.45H19.25L12.375 2.7V9.45Z"
+                            fill="#0076D5" />
+                    </svg>
+                    <h5>Formulaire de signalement d'une situation dangereuse</h5>
+                </a>
+                <a href="" class="form-it">
+                    <svg width="22" height="27" viewBox="0 0 22 27" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M9.625 21.6H12.375V17.55H16.5V14.85H12.375V10.8H9.625V14.85H5.5V17.55H9.625V21.6ZM2.75 27C1.99375 27 1.34613 26.7354 0.807127 26.2062C0.268127 25.677 -0.000914332 25.0416 2.33447e-06 24.3V2.7C2.33447e-06 1.9575 0.269502 1.32165 0.808502 0.792452C1.3475 0.263252 1.99467 -0.000897708 2.75 2.29202e-06H13.75L22 8.1V24.3C22 25.0425 21.7305 25.6783 21.1915 26.2075C20.6525 26.7367 20.0053 27.0009 19.25 27H2.75ZM12.375 9.45H19.25L12.375 2.7V9.45Z"
+                            fill="#0076D5" />
+                    </svg>
+                    <h5>formulaire d'atelier mécanique</h5>
+                </a>
 
+            </div>
 
-<section style="">
-    <h1 style="">Welcome to the Homepage</h1>
-    <p style="">
-        This is a simple homepage. You can navigate to different sections of the website from here.
-    </p>
-    <ul style="">
-        <li ><a href="/" style="text-decoration: none; color: #007bff;">Home</a></li>
-        <li style=""><a href="/connexion" style="text-decoration: none; color: #007bff;">Connexion</a></li>
-        <li style=""><a href="/notifications" style="text-decoration: none; color: #007bff;">Notifications</a></li>
-        <li style=""><a href="/ajoutFormulaire" style="text-decoration: none; color: #007bff;">Ajout Formulaire</a></li>
-        <li style=""><a href="/formulaires" style="text-decoration: none; color: #007bff;">Formulaires</a></li>
-    </ul>
-</section>
+            <h4 style="">Liens procédures</h4>
+            <div class="liens-home">
+                <a href="" class="lien-it">
+                    <svg width="22" height="27" viewBox="0 0 22 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M9.625 21.6H12.375V17.55H16.5V14.85H12.375V10.8H9.625V14.85H5.5V17.55H9.625V21.6ZM2.75 27C1.99375 27 1.34613 26.7354 0.807127 26.2062C0.268127 25.677 -0.000914332 25.0416 2.33447e-06 24.3V2.7C2.33447e-06 1.9575 0.269502 1.32165 0.808502 0.792452C1.3475 0.263252 1.99467 -0.000897708 2.75 2.29202e-06H13.75L22 8.1V24.3C22 25.0425 21.7305 25.6783 21.1915 26.2075C20.6525 26.7367 20.0053 27.0009 19.25 27H2.75ZM12.375 9.45H19.25L12.375 2.7V9.45Z"
+                            fill="#0076D5" />
+                    </svg>
+                    <h5>Hygienne et salubrité</h5>
+                </a>
+                <a href="" class="lien-it">
+                    <svg width="22" height="27" viewBox="0 0 22 27" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M9.625 21.6H12.375V17.55H16.5V14.85H12.375V10.8H9.625V14.85H5.5V17.55H9.625V21.6ZM2.75 27C1.99375 27 1.34613 26.7354 0.807127 26.2062C0.268127 25.677 -0.000914332 25.0416 2.33447e-06 24.3V2.7C2.33447e-06 1.9575 0.269502 1.32165 0.808502 0.792452C1.3475 0.263252 1.99467 -0.000897708 2.75 2.29202e-06H13.75L22 8.1V24.3C22 25.0425 21.7305 25.6783 21.1915 26.2075C20.6525 26.7367 20.0053 27.0009 19.25 27H2.75ZM12.375 9.45H19.25L12.375 2.7V9.45Z"
+                            fill="#0076D5" />
+                    </svg>
+                    <h5>Congé maternité</h5>
+                </a>
+                <a href="" class="lien-it">
+                    <svg width="22" height="27" viewBox="0 0 22 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M9.625 21.6H12.375V17.55H16.5V14.85H12.375V10.8H9.625V14.85H5.5V17.55H9.625V21.6ZM2.75 27C1.99375 27 1.34613 26.7354 0.807127 26.2062C0.268127 25.677 -0.000914332 25.0416 2.33447e-06 24.3V2.7C2.33447e-06 1.9575 0.269502 1.32165 0.808502 0.792452C1.3475 0.263252 1.99467 -0.000897708 2.75 2.29202e-06H13.75L22 8.1V24.3C22 25.0425 21.7305 25.6783 21.1915 26.2075C20.6525 26.7367 20.0053 27.0009 19.25 27H2.75ZM12.375 9.45H19.25L12.375 2.7V9.45Z"
+                            fill="#0076D5" />
+                    </svg>
+                    <h5>Hygienne et salubrité</h5>
+                </a>
+                <a href="" class="lien-it">
+                    <svg width="22" height="27" viewBox="0 0 22 27" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M9.625 21.6H12.375V17.55H16.5V14.85H12.375V10.8H9.625V14.85H5.5V17.55H9.625V21.6ZM2.75 27C1.99375 27 1.34613 26.7354 0.807127 26.2062C0.268127 25.677 -0.000914332 25.0416 2.33447e-06 24.3V2.7C2.33447e-06 1.9575 0.269502 1.32165 0.808502 0.792452C1.3475 0.263252 1.99467 -0.000897708 2.75 2.29202e-06H13.75L22 8.1V24.3C22 25.0425 21.7305 25.6783 21.1915 26.2075C20.6525 26.7367 20.0053 27.0009 19.25 27H2.75ZM12.375 9.45H19.25L12.375 2.7V9.45Z"
+                            fill="#0076D5" />
+                    </svg>
+                    <h5>Congé maternité</h5>
+                </a>
 
+            </div>
+        </div>
+    </section>
 @endsection
