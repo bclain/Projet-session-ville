d869f5027bc5fe678a27586ca2e093aa3b0db8b0
Brice Clain <br.clain@gmail.com>
2023-09-22 14:34:13 -0400

    Update index.blade.php

--- session-projet/resources/views/users/index.blade.php
+++ session-projet/resources/views/users/index.blade.php
@@ -83,24 +83,7 @@
 
             </div>
         </div>
-        {{-- @foreach($notifications as $notification)
-            @php
-                $data = json_decode($notification->data, true);
-            @endphp
-            <li style="margin-bottom: 20px; padding: 15px; background-color: {{ $notification->vu ? '#f4f4f4' : '#e6f7ff' }}; border-radius: 8px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
-                <div style="display: flex; justify-content: space-between;">
-                    <div>
-                        <strong style="color: #343a40;">User ID:</strong> <span style="color: #495057;">{{ $notification->id_user }}</span><br>
-                        <strong style="color: #343a40;">Message:</strong> <span style="color: #495057;">{{ $data['message'] }}</span><br>
-                        <strong style="color: #343a40;">Type:</strong> <span style="color: {{ $data['type'] === 'info' ? '#17a2b8' : '#dc3545' }};">{{ ucfirst($data['type']) }}</span><br>
-                        <strong style="color: #343a40;">Seen:</strong> <span style="color: {{ $notification->vu ? '#28a745' : '#dc3545' }};">{{ $notification->vu ? 'Yes' : 'No' }}</span><br>
-                    </div>
-                    <div>
-                        <strong style="color: #343a40;">Timestamp:</strong> <span style="color: #495057;">{{ $notification->created_at }}</span>
-                    </div>
-                </div>
-            </li>
-        @endforeach
+
 
         
 
